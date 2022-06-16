e7036085290a9c2a9a40e7d4ab5aee36a46aa21b
n3us3r <94902043+n3us3r@users.noreply.github.com>
2022-06-16 23:39:04 +0200

    Prepared for review

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
+import React from 'react';
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendsList from './FriendsList/FriendsList';
+import TransactionHistory from './Transactions/TransactionHistory';
+
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '10vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101'
+    // }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendsList friends={friends} />
+      <TransactionHistory elements={transactions} />;
     </div>
   );
 };
+
+export default App;
